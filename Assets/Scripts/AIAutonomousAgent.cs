899f651eb001d71fa53f5682107479458d0c6f87
bethompson04 <benniert06@gmail.com>
2024-01-09 18:44:27 -0700

    Initial Publish

---
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AIAutonomousAgent : AIAgent
+{
+    public AIPerception perception = null;
+
+    public void Update()
+    {
+        
+    }
+}
